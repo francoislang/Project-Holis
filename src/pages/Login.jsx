60701baefe4475bd1de7f268ac8afa8bcb600018
francois <langfrancoisfrederic@gmail.com>
2022-11-16 13:32:05 +0100

    add last update login page front

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
+import "../style/Login.css"
 
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 export default function Login() {
   return (
-    <div className='d-flex flex-column align-items-center'>
-      <Navbar className="fixed-top" />
-      <main className="form-signin w-100 m-auto d-flex justify-content-center ">
-     
+    <div className='d-flex flex-column container-login'>
+      <main className="form-signin w-100 m-auto d-flex justify-content-center">
+    
       <form className='col-3'>
         <img className="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
         <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
